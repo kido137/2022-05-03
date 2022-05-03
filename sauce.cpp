6d5d68839fe2800d6ca97f5b66ecdd51325b543c
CzDelta <kido137@naver.com>
2022-05-03 17:23:03 +0900

    파일로 쪼개기

--- sauce.cpp
+++ sauce.cpp
@@ -1,27 +1,12 @@
-#include <iostream>
 #include "Common.h"
 #include "Initialize.h"
-
-
-using namespace std;
-
-
-
-
-
-
-
-//declaration
-void Shuffle();
-
-void Split();
-
-void Draw();
+#include "Shuffle.h"
+#include "Split.h"
+#include "Draw.h"
 
 
 int main()
 {
-
 	Initialize();
 
 	Shuffle();
@@ -31,78 +16,4 @@ int main()
 	Draw();
 
 	return 0;
-}
-
-
-
-//implementation
-void Shuffle()
-{
-	for (int i = 0; i < MAX * 2; ++i)
-	{
-		int FirstNumber = rand() % MAX;
-		int SecondNumber = rand() % MAX;
-
-		//Swap
-		int Temp = A[FirstNumber];
-		A[FirstNumber] = A[SecondNumber];
-		A[SecondNumber] = Temp;
-	}
-}
-
-void Split()
-{
-	for (int i = 0; i < 4; ++i)
-	{
-		SplitCard[i] = A[i];
-	}
-}
-
-
-void Draw()
-{
-	cout << "Player" << '\t' << "  AI" << endl;
-	for (int i = 0; i < 4; ++i)
-	{
-		int Type = SplitCard[i] / 13;
-		int Number = SplitCard[i] % 13;
-
-		switch (Type)
-		{
-		case 0:
-			cout << "하트";
-			break;
-		case 1:
-			cout << "스페이드";
-			break;
-		case 2:
-			cout << "클로버";
-			break;
-		case 3:
-			cout << "다이아";
-			break;
-		}
-
-		switch (Number)
-		{
-		case 12:
-			cout << "K" << " ";
-			break;
-		case 11:
-			cout << "Q" << " ";
-			break;
-		case 10:
-			cout << "J" << " ";
-			break;
-		case 0:
-			cout << "A" << " ";
-			break;
-		default:
-			cout << Number + 1 << " ";
-			break;
-		}
-
-		cout << ((i % 2 == 0) ? '\t' : '\n');
-	}
-
 }
